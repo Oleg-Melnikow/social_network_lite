0fc3d740ee011dbe5b336a2d7aa6d097e69ac3b4
Oleg-Melnikow <melnikow.olezhka@mail.ru>
2021-04-18 23:45:19 +0300

    add logic for add post

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {ChangeEvent} from "react";
 import style from "./MyPosts.module.css";
 import {addPost, onPostChange, PostType} from "../../../redux/profileReducer";
 import {AppStateType} from "../../../redux/store";
@@ -6,14 +6,19 @@ import {connect} from "react-redux";
 import {Post} from "./Post/Post";
 
 export function MyPosts(props: MyPostsPropsType) {
+
+    function onPostChange(e: ChangeEvent<HTMLTextAreaElement>){
+        props.onPostChange(e.currentTarget.value)
+    }
+
     return (
         <div className={style.item}>
             <h3>My posts</h3>
             <div>
-                <textarea name="post" id="newPost"/>
+                <textarea name="post" id="newPost" value={props.newPostText} onChange={onPostChange}/>
             </div>
             <div>
-                <button>Add post</button>
+                <button onClick={props.addPost}>Add post</button>
             </div>
             <div>New post</div>
             {props.posts.map(post => <Post key={post.id} id={post.id} avatar={post.avatar} name={post.name}
