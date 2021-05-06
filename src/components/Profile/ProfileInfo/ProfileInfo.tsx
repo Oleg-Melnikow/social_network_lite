3bd4ade7de5f34aaf516c4ba7ab2c80ed341572e
Oleg-Melnikow <melnikow.olezhka@mail.ru>
2021-05-06 17:07:21 +0300

    add ProfileStatus component

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {ProfilePropsType} from "../Profile";
 import style from "./ProfileInfo.module.css"
+import {ProfileStatus} from "./ProfileStatus";
 
 export function ProfileInfo(props: ProfilePropsType) {
     return (
@@ -8,6 +9,7 @@ export function ProfileInfo(props: ProfilePropsType) {
             <div>
                 <img src="https://pbs.twimg.com/profile_banners/3145195603/1500320606/1500x500" alt="pic"/>
             </div>
+            <ProfileStatus/>
             <div className={style.avatar}>
                 <img src={props.profile?.photos.small} alt=""/>
             </div>
---
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -0,0 +1,32 @@
+import React, {ChangeEvent} from "react";
+
+export class ProfileStatus extends React.Component {
+
+    state = {
+        editMode: false,
+        status: "Hello"
+    }
+
+    activeEditMode = () =>{
+        this.setState({editMode: true})
+    }
+
+    deActiveEditMode = () => {
+        this.setState({editMode: false})
+    }
+
+    onChangeStatus = (e:ChangeEvent<HTMLInputElement>) => {
+        this.setState({status: e.currentTarget.value})
+    }
+
+    render() {
+        return <div>
+            {this.state.editMode ?
+                <input type="text" value={this.state.status}
+                       onChange={this.onChangeStatus}
+                       autoFocus onBlur={this.deActiveEditMode}/>:
+                <span onDoubleClick={this.activeEditMode}>{this.state.status}</span>
+            }
+        </div>
+    }
+}
