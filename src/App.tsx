e280c1775eb6a894b5598d330ce5d7acee8d774f
Oleg-Melnikow <melnikow.olezhka@mail.ru>
2021-06-09 20:36:19 +0300

    add lazy and Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import React, {ComponentType} from 'react';
 import { Route, withRouter } from 'react-router-dom';
 import './App.css';
 import {NavBar} from "./components/NavBar/NavBar";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
 import {AppStateType} from "./redux/store";
@@ -12,6 +10,10 @@ import { compose } from 'redux';
 import {connect} from "react-redux";
 import {initializeApp} from "./redux/appReducer";
 import {Preloader} from "./components/common/Preloader/Preloader";
+import {withSuspense} from "./hoc/withSuspense";
+
+const DialogsContainer = React.lazy(() => import("./components/Dialogs/DialogsContainer"));
+const ProfileContainer = React.lazy(() => import("./components/Profile/ProfileContainer"));
 
 class App extends React.Component<AppPropType> {
 
@@ -30,8 +32,8 @@ class App extends React.Component<AppPropType> {
                 <HeaderContainer/>
                 <NavBar/>
                 <div className="wrap-content">
-                    <Route path="/profile/:userId?" render={() => <ProfileContainer/>}/>
-                    <Route path="/dialogs" render={() => <DialogsContainer/>}/>
+                    <Route path="/profile/:userId?" render={() => withSuspense(ProfileContainer)}/>
+                    <Route path="/dialogs" render={() =>withSuspense(DialogsContainer)}/>
                     <Route path="/users" render={() => <UsersContainer/>}/>
                     <Route path="/login" render={() => <Login/>}/>
                 </div>
---
+++ src/hoc/withSuspense.tsx
@@ -0,0 +1,9 @@
+import React, {ComponentType, Suspense} from "react";
+import {Preloader} from "../components/common/Preloader/Preloader";
+
+
+export function withSuspense(Component: ComponentType) {
+    return <Suspense fallback={<Preloader/>}>
+            <Component/>
+        </Suspense>
+}
